4ff4372079e39c519040ab93a9e037a750c850b2
badryalimustofa <125412863+badryalimustofa@users.noreply.github.com>
2023-05-30 16:32:23 +0700

    Add files via upload

--- proses-tambah.php
+++ proses-tambah.php
@@ -1,13 +1,32 @@
 <?php
 include_once("koneksi.php");
+if (isset($_FILES['gambar'])) {
+    $file = $_FILES['gambar'];
 
-$namaDepan = $_POST['nama_depan'];
-$namaBelakang = $_POST['nama_belakang'];
-$surel = $_POST['surel'];
-$noTlp = $_POST['no_telp'];
-$alamat = $_POST['alamat'];
+    // Informasi tentang file
+    $namaFile = $file['name'];
+    $ukuranFile = $file['size'];
+    $tmpFile = $file['tmp_name'];
+    $error = $file['error'];
 
-$result = mysqli_query($conn, "INSERT INTO `customer` (`first_name`,`last_name`,`email`,`phone`,`address`) VALUES ('$namaDepan','$namaBelakang','$surel','$noTlp','$alamat')");
+    // Pindahkan file ke lokasi yang diinginkan
+    if ($error === UPLOAD_ERR_OK) {
+        $tujuan = 'folder-tujuan/' . $namaFile;
+        move_uploaded_file($tmpFile, $tujuan);
+        echo "Gambar berhasil diunggah.";
+    } else {
+        echo "Terjadi kesalahan dalam unggah gambar.";
+    }
+}
+
+$id = $_POST['id'];
+$name = $_POST['name'];
+$categories_id = $_POST['categories_id'];
+$price = $_POST['price'];
+$picture = $_POST['picture'];
+$size = $_POST['size'];
+
+$result = mysqli_query($conn, "INSERT INTO `shoes` (`id`,`name`,`categories_id`,`price`,`picture`,`size`) VALUES ('$id','$name','$categories_id','$price','$picture','$size')");
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
--- proses-ubah.php
+++ proses-ubah.php
@@ -3,13 +3,14 @@ include_once("koneksi.php");
 
 $id = $_GET["id"];
 
-$namaDepan = $_POST['nama_depan'];
-$namaBelakang = $_POST['nama_belakang'];
-$surel = $_POST['surel'];
-$noTlp = $_POST['no_telp'];
-$alamat = $_POST['alamat'];
+$id = $_POST['id'];
+$name = $_POST['name'];
+$categories_id = $_POST['categories_id'];
+$price = $_POST['price'];
+$picture = $_POST['picture'];
+$size = $_POST['size'];
 
-$result = mysqli_query($conn, "UPDATE `customer` SET first_name='$namaDepan',last_name='$namaBelakang',email='$surel',phone='$noTlp', address='$alamat' WHERE id='$id';");
+$result = mysqli_query($conn, "UPDATE `shoes` SET id='$id',name='$name',categories_id='$categories_id',price='$price', picture='$picture', size='$size' WHERE id='$id';");
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
